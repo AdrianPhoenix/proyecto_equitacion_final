be852e55dd77dcf3c31ea38957289a2b688cede7
AdrianPhoenix <jesusadrian.mr97@gmail.com>
2025-06-14 23:35:15 -0400

    Update

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export {default as Home} from "./Home";
-export {default as Login} from "./Login";
-export {default as EquitacionPage} from "./EquitacionPage";
-export {default as EquinoterapiaPage} from "./EquinoterapiaPage";
-
-export {default as Dashboard} from "./dashboard/Dashboard";
-
-export {default as NotFoundPage} from "./NotFoundPage";
